596dc844800e2eea336fe2d0195964f64aaa6fef
Mansi9823 <mansiyadav2962000@gmail.com>
2023-06-03 15:07:10 +0530

    Print login

--- expenses/proxyOptions.js
+++ expenses/proxyOptions.js
@@ -2,7 +2,7 @@ const common_site_config = require('../../../sites/common_site_config.json');
 const { webserver_port } = common_site_config;
 
 export default {
-	'^/(app|api|assets|files)': {
+	'^/(app|api|assets|files|private)': {
 		target: `http://localhost:${webserver_port}`,
 		ws: true,
 		router: function(req) {
